9a83a95817ee04ae231149e0cd58d49f970d0c76
vanessaReact <vanessaReact@gmail.com>
2021-11-21 10:54:30 -0500

    db remote

--- src/config/settings.py
+++ src/config/settings.py
@@ -9,7 +9,10 @@ from sqlalchemy import create_engine
 
 
 # Configure our Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:adMin#12@localhost:3306/encuestas'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:adMin#12@localhost:3306/encuestas'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://b9856c4e2b9364:cb17e2e2@us-cdbr-east-04.cleardb.com/heroku_f83b0e1ea8f66b4'
+
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
